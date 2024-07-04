21d2c16e6205aa9c1751f85e3cfc9e7835710073
Shuang <shuangs3@illinois.edu>
2024-07-04 10:32:37 -0500

    loss.py:use pos_weight and avg for utility loss

--- loss.py
+++ loss.py
@@ -2,9 +2,11 @@ import torch
 import torch.nn as nn
 
 class UtilityLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, pos_weight=1):
         super(UtilityLoss, self).__init__()
+        self.pos_weight = pos_weight
 
     def forward(self, outputs, targets, u_weights):
-        loss = -torch.sum((1 - outputs) * u_weights[:, 0] + outputs * u_weights[:, 1])
-        return loss
+        loss = -((1 - outputs) * u_weights[:, 0] + outputs * u_weights[:, 1])
+        loss += self.pos_weight * targets * loss
+        return torch.sum(loss) / len(outputs)
